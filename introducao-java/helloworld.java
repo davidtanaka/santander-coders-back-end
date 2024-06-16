d0064a0ecf7c596d6f53d7e2d671056a35d1f7ba
Bowie <134816317+davidtanaka@users.noreply.github.com>
2024-06-16 12:42:45 -0300

    Primeiro comando com java

---
+++ introducao-java/helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+
+     public static void main(String[] args) {
+          // write your code here
+
+          String nome;
+          nome = "Davi";
+          System.out.println("Hello, " + nome);
+     }
+}
