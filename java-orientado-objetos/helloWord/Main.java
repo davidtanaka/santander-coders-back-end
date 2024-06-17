ce3389ba4320c02ef5ae9f541e93b9251a32fa50
Bowie <134816317+davidtanaka@users.noreply.github.com>
2024-06-17 13:30:00 -0300

    Inicio de Java orientado a objetos

---
+++ java-orientado-objetos/helloWord/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[]  args) {
+        System.out.println("Hello word!");
+
+        byte variavelByte = 120;
+        System.out.println(variavelByte);
+
+        //short -32768 a 32767
+        // char 0 a 65535
+        // int -2b a 2b
+        // long -9t a 9t
+
+        int variavelInt = 156278;
+        System.out.println(variavelInt);
+
+    }
+}
