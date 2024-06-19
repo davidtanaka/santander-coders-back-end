777cf45357fa42cd2b0d6ca52cb6104d7add7dae
Bowie <134816317+davidtanaka@users.noreply.github.com>
2024-06-19 17:07:10 -0300

    Criando método para remover o ultimo elemento(removeLast)

--- estrutura-de-dados/dataStructure/LinkedList.java
+++ estrutura-de-dados/dataStructure/LinkedList.java
@@ -75,13 +75,40 @@ public class LinkedList {
           length ++;
      }
 
+
+     public Node removeLast() {
+          if (length == 0) return null;
+          Node pre = head;
+          Node temp = null;
+
+          while(pre.next != tail) {
+               pre = pre.next;
+          }
+
+          temp = tail;
+          tail = pre;
+          tail.next = null;
+
+          length--;
+          if (length == 0) {
+               head = null;
+               tail = null;
+          }
+          return temp;
+     }
+
+
      public static void main(String[] args) {
           LinkedList list = new LinkedList("Elemento 1");
           list.append("Elemento 2");
           list.append("Elemento 3");
-          list.getHead();
-          list.getTail();
-          list.getLength();
+
+          System.out.println(list.removeLast().data);
           list.print();
+
+          // list.getHead();
+          // list.getTail();
+          // list.getLength();
+          // list.print();
      }
 }
